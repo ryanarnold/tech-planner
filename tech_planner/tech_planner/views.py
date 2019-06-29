01afba19a976a2ba8d538a3c7c2daf56658c8a5c
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2019-06-29 21:00:36 +0800

    Add functionality for adding categories

--- tech_planner/tech_planner/views.py
+++ tech_planner/tech_planner/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from .models import Category, Product, Build, BuildProduct
+
+# Misc Constants
+HTTP_POST = 'POST'
+
 # URLs
 BUILDS_URL = 'builds'
+CATEGORIES_URL = 'categories'
 
 # Templates
 BUILDS_HTML = 'builds.html'
@@ -24,4 +30,19 @@ def products(request):
 
 
 def categories(request):
-    return render(request, CATEGORIES_HTML)
+    if request.method == HTTP_POST:
+        category_name = request.POST.get('name')
+
+        category = Category.objects.create(
+            name=category_name
+        )
+
+        return HttpResponseRedirect(reverse(CATEGORIES_URL))
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories
+    }
+    
+    return render(request, CATEGORIES_HTML, context)
