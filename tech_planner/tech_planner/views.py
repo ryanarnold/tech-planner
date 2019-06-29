e57266d0d6b30865e8f7bb69bc0d7aea1ffe8ad6
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2019-06-29 19:34:50 +0800

    Create urls and views for index and builds pages

--- tech_planner/tech_planner/views.py
+++ tech_planner/tech_planner/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+# URLs
+BUILDS_URL = 'builds'
+
+# Templates
+BUILDS_HTML = 'builds.html'
+
+def index(request):
+    return HttpResponseRedirect(reverse(BUILDS_URL))
+
+
+def builds(request):
+    return render(request, BUILDS_HTML)
