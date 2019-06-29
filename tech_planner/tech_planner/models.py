81c1a3de0926fa4cc503bda6806f5a9b688a8930
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2019-06-29 20:50:14 +0800

    Create models

--- tech_planner/tech_planner/models.py
+++ tech_planner/tech_planner/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db.models import (Model, ForeignKey, DecimalField, CASCADE, 
+    CharField)
 
-# Create your models here.
+
+class Category(Model):
+    name = CharField(max_length=500)
+
+
+class Product(Model):
+    name = CharField(max_length=500)
+    category = ForeignKey(Category, on_delete=CASCADE)
+    price = DecimalField(max_digits=6, decimal_places=2)
+
+
+class Build(Model):
+    name = CharField(max_length=500)
+
+
+class BuildProduct(Model):
+    status_codes = (
+        ('BOUGHT', 'BOUGHT'),
+        ('TO-BUY', 'TO-BUY')
+    )
+
+    build = ForeignKey(Build, on_delete=CASCADE)
+    product = ForeignKey(Product, on_delete=CASCADE)
+    status_code = CharField(max_length=50, choices=status_codes)
