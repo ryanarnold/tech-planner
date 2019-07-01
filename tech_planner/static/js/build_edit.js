128d47c0b49590d0ef1c9a8b4d473231f0d80012
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2019-07-02 00:08:47 +0800

    Add js code to move to update status url when status is changed

--- tech_planner/static/js/build_edit.js
+++ tech_planner/static/js/build_edit.js
@@ -5,20 +5,34 @@ var categorySelected;
 // Selectors
 var $inputCategory = $("#input-category");
 var $inputProduct = $("#input-product");
+var $inputStatus = $(".input-status");
 
 $inputCategory.change(function(event) {
-    categorySelected = $inputCategory.val();
-    
-    $.ajax({
-        url: "/get-products-using-category/" + categorySelected + "/",
-        success: function(result) {
-            $inputProduct.html("<option selected>Choose...</option>");
-            $.each(result, function(index, item) {
-                var id = item.id;
-                var name = item.name;
+  categorySelected = $inputCategory.val();
+  
+  $.ajax({
+    url: "/get-products-using-category/" + categorySelected + "/",
+    success: function(result) {
+      $inputProduct.html("<option selected>Choose...</option>");
+      $.each(result, function(index, item) {
+        var id = item.id;
+        var name = item.name;
 
-                $inputProduct.append("<option value=\"" + id + "\">" + item.name + "</option>");
-            });
-        }
-    });
+        $inputProduct.append("<option value=\"" + id + "\">" + item.name + "</option>");
+      });
+    }
+  });
+});
+
+$inputStatus.change(function(event) {
+  var updateStatusPath = "/update-build-product-status/";
+  
+  var eventTargetId = event.target.id
+  var buildProductId = eventTargetId.replace("build-product-", "")
+  updateStatusPath += buildProductId + "/";
+  
+  var newStatus = event.target.value;
+  updateStatusPath += newStatus + "/";
+
+  window.location.href = updateStatusPath;
 });
